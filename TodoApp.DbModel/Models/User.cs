b1debd604221d337ed443152c247e16c9459394a
Mustafa_Salah <101256406+mustafasalah21@users.noreply.github.com>
2022-10-14 17:02:10 +0300

    Add files via upload

---
+++ TodoApp.DbModel/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+#nullable disable
+
+namespace TodoApp.DbModel.Models
+{
+    public partial class User
+    {
+        public User()
+        {
+            Todos = new HashSet<Todo>();
+        }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Image { get; set; }
+        public bool IsAdmin { get; set; }
+        public bool IsArchived { get; set; }
+
+        [Timestamp]
+        public DateTime CreatedDateUtcTime { get; set; }
+
+        [Timestamp]
+        public DateTime UpdatedDateUtcTime { get; set; }
+        public virtual ICollection<Todo> Todos { get; set; }
+    }
+}
